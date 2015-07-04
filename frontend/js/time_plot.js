7bb6ceac904031540845a77978967bef28073abf
draganmarjanovic <gagalug13@gmail.com>
2015-07-04 23:27:49 +1000

    Time Plot Data Loading
    Time Plot now successfully loads data.

--- frontend/js/time_plot.js
+++ frontend/js/time_plot.js
@@ -17,108 +17,8 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
 
-
-var test_data = {
-  "description": "[{str(time),int(casualties)}]",
-  "data": [
-    {
-      "time": 01,
-      "fatality_count": 25
-    },
-    {
-      "time": 02,
-      "fatality_count": 25
-    },
-    {
-      "time": 03,
-      "fatality_count": 25
-    },
-    {
-      "time": 04,
-      "fatality_count": 5
-    },
-    {
-      "time": 05,
-      "fatality_count": 10
-    },
-    {
-      "time": 06,
-      "fatality_count": 10
-    },
-    {
-      "time": 07,
-      "fatality_count": 25
-    },
-    {
-      "time": 08,
-      "fatality_count": 25
-    },
-    {
-      "time": 09,
-      "fatality_count": 25
-    },
-    {
-      "time": 10,
-      "fatality_count": 25
-    },
-    {
-      "time": 11,
-      "fatality_count": 25
-    },
-    {
-      "time": 12,
-      "fatality_count": 25
-    },
-    {
-      "time": 13,
-      "fatality_count": 25
-    },
-    {
-      "time": 14,
-      "fatality_count": 25
-    },
-    {
-      "time": 15,
-      "fatality_count": 25
-    },
-    {
-      "time": 16,
-      "fatality_count": 25
-    },
-    {
-      "time": 17,
-      "fatality_count": 25
-    },
-    {
-      "time": 18,
-      "fatality_count": 25
-    },
-    {
-      "time": 19,
-      "fatality_count": 25
-    },
-    {
-      "time": 20,
-      "fatality_count": 25
-    },
-    {
-      "time": 21,
-      "fatality_count": 25
-    },
-    {
-      "time": 22,
-      "fatality_count": 25
-    },
-    {
-      "time": 23,
-      "fatality_count": 25
-    },
-    {
-      "time": 24,
-      "fatality_count": 25
-    },
-  ]
-};
+// Variables and Constants
+var API_PATH = "json/";
 
 
 function prepare_time_data (json_time_data) {
@@ -138,17 +38,21 @@ function prepare_time_data (json_time_data) {
         prepared_data["labels"].push(data[i]["time"]);
         prepared_data["datasets"][0]["data"].push(data[i]["fatality_count"]);
     }
-
-    console.log(prepared_data);
     return(prepared_data);
 }
 
-var barChartData = prepare_time_data(test_data);
+function get_time_data (path) {
+    $.getJSON(path, function( return_feed ) {
+            // console.log(return_feed);
+            var test = prepare_time_data(return_feed);
+            var ctx1 = document.getElementById("time_canvas").getContext("2d");
+    window.myBar = new Chart(ctx1).Bar(test, {
+      responsive : true
+    });
+    });
+}
 
 
+var barChartData = get_time_data(API_PATH);
 
-
-
-
-
-
+console.log(barChartData);
